269b17bca808dba9bcb52b12a2f24a40dc88e4e7
Anatolii Kostin <akostin@atlassian.com>
2023-06-17 18:48:45 +0200

    implemented currentUser

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/navbar/Navbar'
 import RegisterModal from './components/modals/RegisterModal'
 import LoginModal from './components/modals/LoginModal'
 import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata = {
   title: 'Airbnb',
@@ -14,18 +15,19 @@ const font = Nunito({
   subsets: ['latin']
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
         <RegisterModal />
         <LoginModal />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>
