110054b6e6dad734edcd3a4a68658217237e71a2
Anatolii Kostin <akostin@atlassian.com>
2023-09-02 21:16:21 +0200

    add favorites page

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -37,7 +37,7 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
-        toast.success("Success!");
+        toast.success("Account created!");
         registerModal.onClose();
         loginModal.onOpen();
       })
